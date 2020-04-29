28e7a3e44adc77b12b97d64b9cf45dd6fc1c2af0
Kei95 <kei0613shine.star@gmail.com>
2020-04-29 15:53:25 -0700

    add a few routes on profile

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+  jsxSingleQuote: false,
+};

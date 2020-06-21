d148b40e10dabc62b6da8e1e4e456eda2868fbbd
Kei95 <kei0613shine.star@gmail.com>
2020-06-21 12:54:01 -0700

    separate routes and add not found page

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,13 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-import Register from './component/auth/Register';
-import Login from './component/auth/Login';
 import Navbar from './component/layout/Navbar';
 import Landing from './component/layout/Landing';
-import Alert from './component/layout/Alert';
-import Dashboard from './component/dashboard/Dashboard';
-import PrivateRoute from './component/routing/PrivateRoute';
-import CreateProfile from './component/profile-form/CreateProfile';
-import EditProfile from './component/profile-form/EditProfile';
-import AddExperience from './component/profile-form/AddExperience';
-import AddEducation from './component/profile-form/AddEducation';
-import Profiles from './component/profiles/Profiles';
-import Profile from './component/profile/Profile';
-import Posts from './component/posts/Posts';
-import Post from './component/posts/Post';
+import Routes from './component/routing/Routes';
 
 // redux
 import { Provider } from 'react-redux';
 import store from './store';
-
 import './App.css';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -42,47 +29,10 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute
-                exact
-                path="/dashboard"
-                component={Dashboard}
-              />
-              <PrivateRoute
-                exact
-                path="/create-profile"
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/edit-profile"
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/add-experience"
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path="/add-education"
-                component={AddEducation}
-              />
-              <PrivateRoute exact path="/posts" component={Posts} />
-              <PrivateRoute
-                exact
-                path="/posts/:id"
-                component={Post}
-              />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
---
+++ client/src/component/layout/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+export default () => {
+  return (
+    <Fragment>
+      <h2 className="x-large text-primary">
+        <i class="fas fa-exclamation-triangle"></i>Page Not Found
+      </h2>
+      <p class="large">Sorry, this page doesn't exist</p>
+    </Fragment>
+  );
+};
